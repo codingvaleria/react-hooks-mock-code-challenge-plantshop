19a4a388326ccc873e78031feacf5df9ac15553c
codingvaleria <codingvaleria@gmail.com>
2022-10-15 18:07:45 +0300

    out of stock toggle

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React,{useState}from "react";
 
 function PlantCard({ plant }) {
+const [isInStock, setisInStock] =useState(true)
+
+function handleBtnClick(e){
+  setisInStock((isInStock)=> !isInStock)
+}
+
   return (
     <li className="card">
       <img src={plant.image} alt={"plant name"} />
       <h4>{plant.name}</h4>
       <p>Price: {plant.price}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      {isInStock ? (
+        <button onClick= {handleBtnClick} className="primary">In Stock</button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick= {handleBtnClick}>Out of Stock</button>
       )}
     </li>
   );
